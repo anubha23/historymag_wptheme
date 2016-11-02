57cdfe1efed99534390e406314fa0e1be90fb67f
Anubha <anubha007@gmail.com>
2016-11-02 22:23:29 +0530

    Made changes to the archive part of front-page.php

--- wordpress/wp-content/themes/historymag/front-page.php
+++ wordpress/wp-content/themes/historymag/front-page.php
@@ -107,7 +107,33 @@ Nehru University</p>
 </div>
 </div>
 <div class="row">
-<div class="col-md-3" style="border-right: 2px solid #eeeeee;">
+<?php
+$args = array(
+    'post_type' => 'post'
+    );
+$query = new WP_Query( $args );
+if ( $query->have_posts() ) :
+
+    while ( $query->have_posts() ) : $query->the_post(); ?>
+	
+	<div class="col-md-3" style="border-right: 2px solid #eeeeee;">
+
+<a class="thumbnail" href="#">
+<img src="http://placekitten.com/200/150" alt="top_story" />
+</a>
+<span style="color: #d02128; font-size: 17.5px;">INDIAN HISTORY</span>
+<p style="font-size: 17.5px;"><?php the_field('quote'); ?></p>
+
+</div>
+    <?php endwhile;
+  wp_reset_postdata();
+ 
+else : 
+?>
+ <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
+
+<!--<div class="col-md-3" style="border-right: 2px solid #eeeeee;">
 
 <a class="thumbnail" href="#">
 <img src="http://placekitten.com/200/150" alt="top_story" />
@@ -150,21 +176,6 @@ history and methods
 of teaching it. The
 emphasis is on dates
 and events.</p>
-
-</div>
-<div class="col-md-3" style="border-right: 2px solid #eeeeee;">
-
-<a class="thumbnail" href="#">
-<img src="http://placekitten.com/200/150" alt="top_story" />
-</a>
-<span style="color: #d02128; font-size: 17.5px;">INDIAN HISTORY</span>
-<p style="font-size: 17.5px;">We are still using a
-completely outdated
-understanding of
-history and methods
-of teaching it. The
-emphasis is on dates
-and events.</p>
-</div>
+</div>-->
 </div>
 </div>
