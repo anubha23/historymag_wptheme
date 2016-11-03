166db49345c65c28c52eba036de729833260e396
Anubha <anubha007@gmail.com>
2016-11-04 00:37:12 +0530

    Added post and news page template

--- wordpress/wp-content/themes/historymag/functions.php
+++ wordpress/wp-content/themes/historymag/functions.php
@@ -14,4 +14,12 @@ function register_my_menu() {
   register_nav_menu('header-menu',__( 'Header Menu' ));
 }
 add_action( 'init', 'register_my_menu' );
+
+if ( function_exists('register_sidebar') )
+	register_sidebar(array(
+		'before_widget' => '',
+		'after_widget' => '',
+		'before_title' => '<h3>',
+		'after_title' => '</h3>',
+	));
 ?>
