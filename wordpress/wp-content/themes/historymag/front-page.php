022a42d617ecc02cbea2852cd0b024ce90463efd
Anubha <anubha007@gmail.com>
2016-11-09 16:28:13 +0530

    Made changes to files, added carousel

--- wordpress/wp-content/themes/historymag/front-page.php
+++ wordpress/wp-content/themes/historymag/front-page.php
@@ -1,50 +1,78 @@
 <?php get_header(); ?>
-<div class="row">
-<div class="col-md-4">
+
 <h3><span class="label label-default">TOP STORIES</span></h3>
-&nbsp;
-<?php 
-/*if ( have_posts() ) {
-	while ( have_posts() ) {
-		the_post(); 
-		the_content();*/
+<!--Carousel-->
+
+<div id="myCarousel" class="carousel slide" data-interval="6000" data-ride="carousel">
+    <!-- Carousel indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>   
+    <!-- Carousel items -->
+    <div class="carousel-inner">
+	
+	<?php
 		
 		$args = array(
     'post_type' => 'post',
 	'category_name'=> 'topstories'
     );
-$query = new WP_Query( $args );
-if ( $query->have_posts() ) :
+	$i=0;
+	$query = new WP_Query( $args );
+	if ( $query->have_posts() ) :
 
-    while ( $query->have_posts() ) : $query->the_post(); ?>
-        <blockquote><?php the_field('quote'); ?></blockquote>
-		<?php the_field('excerpt'); ?>
-    <?php endwhile;
-  wp_reset_postdata();
+    while ( $query->have_posts() ) : $query->the_post(); 
+		if ($i==0) : ?>
+		<div class="item active">
+		<?php else : ?>
+		<div class="item">
+        <?php endif; ?>	
+        <!-- Start designing slide -->
+		<div class="row">
+			<div class="col-md-4">
+			 <blockquote><?php the_field('quote'); ?></blockquote>
+			<?php the_field('excerpt'); ?>
+			</div>
+			
+			<div class="col-md-8">
+			<img src="<?php the_field('featuredimage'); ?>" align="right">
+            <div class="carousel-caption">
+                <h3><?php the_title(); ?></h3>
+            </div>
+			</div>
+			
+		</div>
+		</div>
+		
+		
+		<!--End designing slide-->
+		
+		
+		
+    <?php 
+	$i++;
+	endwhile;
+	wp_reset_postdata(); ?>
+	</div>
+	
+	<!-- Carousel nav -->
+    <a class="carousel-control left" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+    </a>
+    <a class="carousel-control right" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+    </a>
+	</div>
  
-else : ?>
+	<?php else : ?>
     <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-<?php endif; ?>
-	<!--
-<blockquote>‘We are still using a
-completely outdated
-understanding of
-history and methods
-of teaching it. The
-emphasis is on dates
-and events’</blockquote>
--Ranabir Chakravarti interviews
-historian Romila Thapar,
-Professor Emeritus, Jawaharlal
-Nehru University
--->
-</div>
-<div class="col-md-8"><a class="thumbnail" href="#">
-<img src="http://placekitten.com/500/500" alt="top_story" />
-</a></div>
-</div>
-<div class="row">
+	<?php endif; ?>
 
+
+
+<div class="row">
 <div class="col-md-12">
 <h3><span class="label label-default">FEATURED</span></h3>
 </div>
